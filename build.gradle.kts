c886f96dd917b6dedf42905a6297af8aa9800078
Darwin Algarin <dalgarins@live.com>
2023-10-07 12:08:22 -0500

    upgrade to 1.16.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij") version "1.16.0"
     id("org.jetbrains.grammarkit") version "2022.3.2"
 }
 
 group = "co.anbora.labs"
-version = "1.3.1"
+version = "1.3.2"
 
 repositories {
     mavenCentral()
