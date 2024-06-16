045c1627754f3d92f438c6d26281e0cc506d3cc8
Darwin Algarin <dalgarins@live.com>
2024-06-16 12:51:26 -0500

    upgrade sdk v2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,46 @@
+fun properties(key: String) = providers.gradleProperty(key)
+fun environment(key: String) = providers.environmentVariable(key)
+
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.0"
-    id("org.jetbrains.intellij") version "1.17.0"
-    id("org.jetbrains.grammarkit") version "2022.3.2.1"
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
+    id("org.jetbrains.intellij.platform") version "2.0.0-beta7"
+    id("org.jetbrains.grammarkit") version "2022.3.2.2"
 }
 
-group = "co.anbora.labs"
-version = "1.3.5"
+group = properties("pluginGroup").get()
+version = properties("pluginVersion").get()
 
+// Set the JVM language level used to build the project.
+kotlin {
+    jvmToolchain(17)
+}
+
+// Configure project's dependencies
 repositories {
     mavenCentral()
+
+    // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+    intellijPlatform {
+        create(properties("platformType"), properties("platformVersion"))
+
+        // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
+        bundledPlugins(properties("platformBundledPlugins").map { it.split(',') })
+
+        // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
+        plugins(properties("platformPlugins").map { it.split(',') })
+
+        instrumentationTools()
+        pluginVerifier()
+        // testFramework(TestFrameworkType.Platform.JUnit4)
+    }
 }
 
 sourceSets {
@@ -18,32 +49,50 @@ sourceSets {
     }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.3")
-    type.set("IU") // Target IDE Platform
+// Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
+intellijPlatform {
+    pluginConfiguration {
+        version = properties("pluginVersion")
+        description = file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8)
+        changeNotes = file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8)
 
-    plugins.set(listOf())
+        ideaVersion {
+            sinceBuild = properties("pluginSinceBuild")
+            untilBuild = properties("pluginUntilBuild")
+        }
+    }
+
+    publishing {
+        token = environment("PUBLISH_TOKEN")
+        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        channels = properties("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
+    }
+
+    verifyPlugin {
+        ides {
+            recommended()
+        }
+    }
 }
 
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+
+    wrapper {
+        gradleVersion = properties("gradleVersion").get()
     }
 
     val generateFantomLexer = task<org.jetbrains.grammarkit.tasks.GenerateLexerTask>("generateFantomLexer") {
         sourceFile.set(file("src/main/grammar/Fantom.flex"))
-        targetDir.set("src/main/gen/co/anbora/labs/fantom/lang/")
-        targetClass.set("FanLexer")
+        targetOutputDir.set(file("src/main/gen/co/anbora/labs/fantom/lang/"))
+        // targetClass.set("FanLexer")
         purgeOldFiles.set(true)
     }
 
     val generateFantomParser = task<org.jetbrains.grammarkit.tasks.GenerateParserTask>("generateFantomParser") {
         sourceFile.set(file("src/main/grammar/Fantom.bnf"))
-        targetRoot.set("src/main/gen")
+        targetRootOutputDir.set(file("src/main/gen"))
         pathToParser.set("/co/anbora/labs/fantom/lang/core/parser/FantomParser.java")
         pathToPsiRoot.set("/co/anbora/labs/fantom/lang/core/psi")
         purgeOldFiles.set(true)
@@ -51,21 +100,5 @@ tasks {
 
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         dependsOn(generateFantomLexer, generateFantomParser)
-        kotlinOptions.jvmTarget = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("233")
-        untilBuild.set("241.*")
-        changeNotes.set(file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8))
-        pluginDescription.set(file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
-
-    buildSearchableOptions {
-        enabled = false
     }
 }
